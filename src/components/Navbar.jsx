a04a9f19787dc5331c0063acb1788b4c70d6ee3e
Plug-On <plugon035@gmail.com>
2025-02-18 07:13:55 +0545

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import logo from '../assets/logo.png';
+function Navbar(){
+    return(
+        <div>
+            <nav className="flex justify-between items-center px-15 py-2 bg-gray-100">
+            
+                <img src={logo} alt="" className="h-20" />
+                <ul className="flex justify-center space-x-4">
+                    <li><a href="#" className="text-blue-500">Home</a></li>
+                    <li><a href="#" className="text-blue-500">About</a></li>
+                    <li><a href="#" className="text-blue-500">Services</a></li>
+                    <li><a href="#" className="text-blue-500">Contacts</a></li>
+
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
