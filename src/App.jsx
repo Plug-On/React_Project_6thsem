aaa2c7070ea33d03b558d404359deaee28aa6a28
Plug-On <plugon035@gmail.com>
2025-02-25 07:05:17 +0545

    footer missed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import ProductContainer from './components/ProductContainer'
 import ServiceContianer from './components/ServiceContainer'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <ProductContainer/>
       <ServiceContianer/>
+      <Footer/>
     </>
   )
 }
