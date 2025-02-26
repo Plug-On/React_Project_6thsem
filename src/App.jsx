436a7cecad980b45b00704693615c1174b279285
Plug-On <plugon035@gmail.com>
2025-02-26 23:20:08 +0545

    hw

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar'
 import ProductContainer from './components/ProductContainer'
 import ServiceContianer from './components/ServiceContainer'
 import Footer from './components/Footer'
+import About from './components/About'
 
 function App() {
 
@@ -11,7 +12,9 @@ function App() {
       <Navbar/>
       <ProductContainer/>
       <ServiceContianer/>
+      <About/>
       <Footer/>
+      
     </>
   )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ function ProductCard({ product })
             <a href="" className="bg-white p-4 rounded-lg shadow-lg block hover:shadow-xl hover:-translate-y-1 transition duration:300">
             <img src={product.image} alt="product" className="h-40 w-full object-cover" />
             <h2 className="text-lg font-semibold mt-4">{product.productname}</h2>
-            <p className="test-sm mt-2"> {product.description} </p>
+            <p className="text-sm font-semibold mt-2"> {product.description} </p>
             <p className="text-sm font-semibold mt-2">{product.price}</p>
             </a>
         </div>
