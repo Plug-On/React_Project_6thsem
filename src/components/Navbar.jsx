5423bfc876381ac56b7d02a2742131954722e68b
Plug-On <plugon035@gmail.com>
2025-03-05 07:39:00 +0545

    underline

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,16 +8,19 @@ function Navbar(){
                 <img src={logo} alt="" className="h-20" />
                 <ul className="flex justify-center space-x-4">
                     <li>
-                        <NavLink to='/' className={({isActive}) => isActive ? "text-red-500 font-bold" : "text-blue-500"}>Home</NavLink>
+                        <NavLink to='/' className={({isActive}) => isActive ? "text-blue-500 font-bold underline" : "text-blue-500"}>Home</NavLink>
                     </li>
                     <li>                        
-                        <NavLink to='/about' className={({isActive}) => isActive ? "text-red-500 font-bold" : "text-blue-500"}>About</NavLink>
+                        <NavLink to='/about' className={({isActive}) => isActive ? "text-blue-500 font-bold underline" : "text-blue-500"}>About</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/services' className={({isActive}) => isActive ? "text-red-500 font-bold" : "text-blue-500"}>Services</NavLink>
+                        <NavLink to='/services' className={({isActive}) => isActive ? "text-blue-500 font-bold underline" : "text-blue-500"}>Services</NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contacts" className={({isActive}) => isActive ? "text-red-500 font-bold" : "text-blue-500"}>Contacts</NavLink>
+                        <NavLink to="/contacts" className={({isActive}) => isActive ? "text-blue-500 font-bold underline" : "text-blue-500"}>Contacts</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login" className={({isActive}) => isActive ? "text-blue-500 font-bold underline" : "text-blue-500"}>Login</NavLink>
                     </li>
 
                 </ul>
