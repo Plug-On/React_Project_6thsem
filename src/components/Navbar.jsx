6706457f8ca7f4a6de0f4cfb3fff352cd1ccfdf0
Plug-On <plugon035@gmail.com>
2025-04-07 07:05:56 +0545

    using api to import day1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
+import { useEffect, useState } from 'react';
 import logo from '../assets/logo.png';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 function Navbar(){
+
+    const [categories , setCategories] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/categories/')
+          .then((response) => {
+            setCategories(response.data.data);
+          })
+      }, []);
+
     return(
         <div className='sticky top-0 z-50'>
             <nav className="flex justify-between items-center px-10 py-1 bg-gray-100 shadow-lg">
@@ -14,11 +26,15 @@ function Navbar(){
                         <NavLink to='/about' className={({isActive}) => isActive ? "text-blue-500 font-bold overline" : "text-blue-500"}>About</NavLink>
                     </li>
                     <li>
-                        <NavLink to='/services' className={({isActive}) => isActive ? "text-blue-500 font-bold overline" : "text-blue-500"}>Services</NavLink>
+                        <NavLink to='/services' className={({isActive}) => isActive ? "text-blue-500 font-bold overline" : "text-blue-500"}>Categories</NavLink>
                     </li>
-                    <li>
-                        <NavLink to="/contacts" className={({isActive}) => isActive ? "text-blue-500 font-bold overline" : "text-blue-500"}>Contacts</NavLink>
-                    </li>
+                   
+                    {categories.map((category) => (
+                         <li key={category.id} className='text-blue-500'>
+                             {category.name}
+                         </li>
+                     ))}
+
                     <li>
                         <NavLink to="/login" className={({isActive}) => isActive ? "text-blue-500 font-bold overline" : "text-blue-500"}>Login</NavLink>
                     </li>
